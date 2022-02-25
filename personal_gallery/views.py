3dc23641a6a0b515dccfa319eedcd9d5a5d60fbb
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-25 13:13:44 +0300

    gallery route registration

--- personal_gallery/views.py
+++ personal_gallery/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import datetime as dt
+from  .models import Image
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Welcome home')
+    date = dt.date.today()
+    return render(request, 'index.html',{"date": date})
+
+def gallery(request):
+    my_gallery= Image.get_images()
+    print(my_gallery)
+    return render(request, 'gallery.html', {'gallery': my_gallery})
