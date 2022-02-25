3087ca395f43d6b51ad9a4cf36b011999d1a1a1a
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-25 13:10:20 +0300

    apps registration

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'personal_gallery.apps.PersonalGalleryConfig'
+    'personal_gallery.apps.PersonalGalleryConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
