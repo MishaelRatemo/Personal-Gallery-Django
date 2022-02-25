ec231a53a77f572250f86033eef3f706c4b0999f
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-25 13:15:48 +0300

    updates on image uploades and css

--- personal_gallery/urls.py
+++ personal_gallery/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+
 
 
 urlpatterns =[
     path('', views.home, name='index'),
+    path('gallery/', views.gallery, name='gallery')
 
-]
+] 
