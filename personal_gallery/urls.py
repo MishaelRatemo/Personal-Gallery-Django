a7b6475a28799f73552870a024407e9c2293e083
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-28 08:10:41 +0300

    registered about and contact routes in urls

--- personal_gallery/urls.py
+++ personal_gallery/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 from django.conf import settings
@@ -7,6 +8,8 @@ from django.conf import settings
 urlpatterns =[
     path('', views.home, name='index'),
     path('gallery/', views.gallery, name='gallery'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('about/',views.about, name='about'),
+    path('contact/',views.contact, name='contact'),
 
 ] 
